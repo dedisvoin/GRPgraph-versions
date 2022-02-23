3482008cb510c5de6ae8d9e99aee422123e49e73
dedisvoin <88434293+dedisvoin@users.noreply.github.com>
2022-02-23 11:03:53 +0300

    Add files via upload
    Added more new functions.

---
+++ GRPgraph_v15.5.122c3/DATA_TIME.py
@@ -0,0 +1,39 @@
+import datetime
+
+class Da_me:
+    def __init__(self):
+        pass
+    def GET_date(self):
+            date = datetime.date.today() ; return date
+    class GET_DATE:
+                def GET_year():
+                    date = datetime.date.today()
+                    return date.year
+                def GET_day():
+                    date = datetime.date.today()
+                    return date.day
+                def GET_month():
+                    date = datetime.date.today()
+                    return date.month
+
+
+    class GET_TIME:
+                def GET_hour():
+                    date = datetime.datetime.today()
+                    return date.hour
+                def GET_second():
+                    date = datetime.datetime.today()
+                    return date.second
+                def GET_minute():
+                    date = datetime.datetime.today()
+                    return date.minute     
+                def GET_milisec():
+                    date = datetime.datetime.today()
+                    return  date.microsecond
+
+
+
+
+
+
+
---
+++ GRPgraph_v15.5.122c3/__init__.py
@@ -0,0 +1,3 @@
+import colorama
+print(colorama.Fore.BLUE+'GRPgraph version - (15.5.122c3)'+colorama.Fore.RESET)
+print(colorama.Fore.CYAN+colorama.Style.BRIGHT+'My Github --- ( https://github.com/dedisvoin/GRPgraph-versions )'+colorama.Fore.RESET,colorama.Style.RESET_ALL)
